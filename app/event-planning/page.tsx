d237a92a3c53d29118d2e7a5f29a4f4f5d2fdf21
alcalandrea <alcalandrea@gmail.com>
2023-09-04 14:09:34 -0400

    Add "coming soon" text to in progress pages

--- app/event-planning/page.tsx
+++ app/event-planning/page.tsx
@@ -1,3 +1,12 @@
 export default function EventPlanningPage() {
-  return <h1>Hello, Event Planning Page!</h1>
+  return (
+    <main className="flex flex-col items-center justify-center gap-20 px-4">
+      <h1 className="text-xl font-bold sm:text-2xl lg:text-3xl">
+        Event Planning
+      </h1>
+      <h2 className="text-lg font-bold sm:text-xl lg:text-2xl">
+        Coming Soon!
+      </h2>
+    </main>
+  )
 }
