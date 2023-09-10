e43a4e1ff88f23381d159436e597f809bf3e460f
alcalandrea <alcalandrea@gmail.com>
2023-09-10 16:28:51 -0400

    Fix bug caused by invalid syntax

--- app/error.tsx
+++ app/error.tsx
@@ -10,7 +10,7 @@ export default function Error({error}: {error: Error}) {
       <div className="flex flex-col justify-center gap-10 text-center">
         <h1 className="text-xl font-bold sm:text-2xl lg:text-3xl">Oh. No.</h1>
         <h2 className="text-lg font-bold sm:text-xl lg:text-2xl">
-          You weren't supposed to see this 🙈
+          You weren&apos;t supposed to see this 🙈
         </h2>
         <p>{(error as Error)?.message ?? "An unexpected error ocurred"}</p>
         <CoreLink href="/" replace variant="underlined">
